48af30555d5636034305ba1252d131b77bc5130a
uncenter <uncenter@uncenter.dev>
2026-06-05 17:24:41 -0400

    fix: handle style 404s

--- src/routes/style/[user=actor]/[style=rkey]/+page.ts
+++ src/routes/style/[user=actor]/[style=rkey]/+page.ts
@@ -1,9 +1,23 @@
 import type { PageLoad } from './$types';
 import { getProfile, getUserstyle } from '$lib/at';
+import { ClientResponseError } from '@atcute/client';
+import { error } from '@sveltejs/kit';
 
 export const load: PageLoad = async ({ params }) => {
   let { user, style } = params;
-  let userstyle = await getUserstyle(user, style);
-  let profile = await getProfile(user);
-  return { userstyle, profile, user, style };
+  try {
+    let userstyle = await getUserstyle(user, style);
+
+    let profile = await getProfile(user);
+    return { userstyle, profile, user, style };
+  } catch (e) {
+    if (e instanceof ClientResponseError) {
+      switch (e.error) {
+        case 'RecordNotFound':
+          error(404, e.message);
+        default:
+          error(500, e.message)
+      }
+    }
+  }
 };
