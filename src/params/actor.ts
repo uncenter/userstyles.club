ce095b2e4a4481512179813a56e6f9071aa0fd5b
uncenter <uncenter@uncenter.dev>
2026-08-16 23:16:24 -0400

    Allow uri encoded actor identifiers in url path segments

--- src/params/actor.ts
+++ src/params/actor.ts
@@ -1,4 +1,11 @@
 import type { ParamMatcher } from '@sveltejs/kit';
+import type { ActorIdentifier } from '@atcute/lexicons';
 import { isActorIdentifier } from '@atcute/lexicons/syntax';
 
-export const match = isActorIdentifier satisfies ParamMatcher;
+export const match = ((value: string): value is ActorIdentifier => {
+  try {
+    return isActorIdentifier(decodeURIComponent(value));
+  } catch {
+    return false;
+  }
+}) satisfies ParamMatcher;
